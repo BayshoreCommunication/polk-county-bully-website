9e7b8ddf3c0132b016e21852795ef707bb401f75
uiabu <ui.abukawsar@gmail.com>
2025-09-25 17:07:11 +0600

    Refactor EmbeddedSite and Hero components for improved structure and styling; update package-lock.json with correct project name; replace left image with updated version.

--- components/EmbeddedSite.tsx
+++ components/EmbeddedSite.tsx
@@ -1,12 +1,17 @@
 export default function EmbeddedSite() {
-    return (
-
-        <section className="bg-[#F5F9FA] 2xl:pt-[200px] pb-12" >
-            <div className="max-w-[1640px] px-8 mb-8 md:mb-16 mx-auto">
-                <div className="textwidget"><p><iframe loading="lazy" style={{ height: 800, width: "100%" }} src="https://www.wagtopia.com/search/org?id=1233589&iframe=normal"></iframe></p>
-                </div>
-            </div>
-        </section>
-
-    );
+  return (
+    <section className="bg-[#F5F9FA] lg:pt-[200px] pb-12">
+      <div className="max-w-[1640px] px-8 mb-8 md:mb-16 mx-auto">
+        <div className="textwidget">
+          <p>
+            <iframe
+              loading="lazy"
+              style={{ height: 800, width: "100%" }}
+              src="https://www.wagtopia.com/search/org?id=1233589&iframe=normal"
+            ></iframe>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
